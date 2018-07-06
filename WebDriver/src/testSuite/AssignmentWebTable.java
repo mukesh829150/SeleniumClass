b7bcd39a1bd97323bd08826fd2a4122713cacaa5
I I M <I I M@CC-GAURAV-PC>
2018-07-06 15:26:05 +0530

    Assignment Web Table updated - 06072018

--- WebDriver/src/testSuite/AssignmentWebTable.java
+++ WebDriver/src/testSuite/AssignmentWebTable.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -25,13 +26,17 @@ public class AssignmentWebTable extends Browser {
 		
 		for(int i=1;i<=numRows;){
 			//WebElement firstName = driver.findElement(By.xpath("//span[text()='First Name']"));
+			Actions action = new Actions(driver);
 			WebDriverWait wait2=new WebDriverWait(driver,10);
 			wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='ui-grid-cell-contents ng-binding ng-scope'])["+i+"]")));
 			String a = driver.findElement(By.xpath("(//div[@class='ui-grid-cell-contents ng-binding ng-scope'])["+(i+1)+"]")).getText();
 			//System.out.println(a);
 			//System.out.println(i);
 			if(a.equalsIgnoreCase("carlo")){
-				System.out.println("The First Name has been found as :" +a+ " at row " +((i/5)+1));
+				int row = (i/5)+1;
+				WebElement editButton = driver.findElement(By.xpath("(//i[@class='fa fa-pencil'])["+row+"]"));
+				action.doubleClick(editButton).perform();
+				System.out.println("The First Name has been found as : '"+a+"' at row " +row);
 				break;
 			}else if(i == (numRows-4)){
 				driver.findElement(By.xpath("//button[@class='ui-grid-pager-next']")).click();
