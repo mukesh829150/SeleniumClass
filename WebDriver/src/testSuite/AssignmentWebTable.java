f5284abdccf9718a68dab69c1ebb331982b8f68a
user <user@Shelly-PC>
2018-07-08 13:08:01 +0530

    Assignment webtable - 08072018 and Alerts

--- WebDriver/src/testSuite/AssignmentWebTable.java
+++ WebDriver/src/testSuite/AssignmentWebTable.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -12,7 +13,7 @@ import main.Browser;
 
 public class AssignmentWebTable extends Browser {
 
-	@Test
+	@Test(enabled=false)
 	public void webtable() {
 		driver.navigate().to("http://demo.automationtesting.in/WebTable.html");
 		driver.manage().window().maximize();
@@ -71,5 +72,68 @@ public class AssignmentWebTable extends Browser {
 		//WebElement FirstName = driver.findElement(By.xpath("//div[text()='carlo']"));
 
 	}
+	
+	
+	
+	@Test
+	public void SearchTables() throws InterruptedException {
+		driver.navigate().to("http://demo.automationtesting.in/WebTable.html");
+		driver.manage().window().maximize();
+		
+		WebDriverWait wait=new WebDriverWait(driver,20);
+		Actions action = new Actions(driver);
+		WebElement btnNext=null;
+		List<WebElement> rowsList=null;
+		WebElement firstName = null, editButton = null;
+		String txtFirstName = null;
+		
+		try{
+			btnNext=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='ui-grid-pager-next']")));
+		//btnNext=driver.findElement(By.xpath("//button[@class='ui-grid-pager-next']"));
+		}catch(Exception e){
+			System.out.println("Next button is Not present in the table");
+		}
+		
+		try{
+			rowsList=wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='ui-grid-row ng-scope']")));	
+			//rowsList=driver.findElements(By.xpath("//div[@class='ui-grid-row ng-scope']"));
+		}catch(Exception e){
+			System.out.println("table is not there");
+		}
+		
+		String result = "fail";
+		//while(btnNext.isEnabled()){
+		do{
+			
+			for(int i=1;i<=rowsList.size();i++){
+				try {
+					firstName=	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='ui-grid-row ng-scope']["+i+"]/div/div[2]")));
+				//	firstName = driver.findElement(By.xpath("//div[@class='ui-grid-row ng-scope']["+i+"]/div/div[2]"));
+					txtFirstName = firstName.getText();
+				} catch (Exception e) {
+					System.out.println("First Name Column is not found");
+				}
+				if(txtFirstName.equalsIgnoreCase("carlo")){
+					editButton = driver.findElement(By.xpath("(//i[@class='fa fa-pencil'])["+i+"]"));
+					action.doubleClick(editButton).build().perform();
+					//action.moveToElement(editButton).doubleClick().build().perform();
+					System.out.println("FirstName found.");
+					Thread.sleep(3000);
+					firstName.findElement(By.xpath(".//input")).clear();
+					firstName.findElement(By.xpath(".//input")).sendKeys("Gaurav");
+					//driver.findElement(By.xpath("//div[@class='ui-grid-row ng-scope']["+i+"]/div/div[2]//input")).clear();
+					//driver.findElement(By.xpath("//div[@class='ui-grid-row ng-scope']["+i+"]/div/div[2]//input")).sendKeys("Gaurav");
+					result = "pass";
+					break;
+				}
+			}
+			if(result.equalsIgnoreCase("pass")){
+				break;
+			}else{
+				btnNext.click();	
+			}
 
+		   }while(btnNext.isEnabled());
+		
+	}
 }
