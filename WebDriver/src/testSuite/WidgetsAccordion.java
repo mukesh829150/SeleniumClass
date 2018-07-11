fce355eb687dac0144eb5479f0b272e3ff2a0245
user <user@Shelly-PC>
2018-07-12 02:50:28 +0530

    accordion text print

---
+++ WebDriver/src/testSuite/WidgetsAccordion.java
@@ -0,0 +1,31 @@
+package testSuite;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import main.Browser;
+
+public class WidgetsAccordion extends Browser{
+	
+	@Test
+	public void testAccordion(){
+		driver.navigate().to("http://demo.automationtesting.in/Accordion.html");
+		driver.manage().window().maximize();
+		
+		List<WebElement> totalAccords = driver.findElements(By.xpath("//a[@data-toggle='collapse']/b"));
+		int totalnoAccords = totalAccords.size();
+		System.out.println(totalnoAccords);
+		
+		for(int i = 1; i <= totalnoAccords; i++){
+			String linkName = driver.findElement(By.xpath("//a[@href='#collapse"+i+"']/b")).getText();
+			System.out.println(linkName);
+			driver.findElement(By.xpath("//a[@href='#collapse"+i+"']/b")).click();
+			String insideText = driver.findElement(By.xpath("//div[@id='collapse"+i+"']/div")).getText();
+			System.out.println(insideText);
+		}
+	}
+
+}
