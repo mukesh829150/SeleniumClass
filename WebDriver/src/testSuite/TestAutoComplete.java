66bb45db53c8e9396527a722b61836dce9c779ae
Shelly-PC <Shelly-PC@Shelly-PC>
2018-07-29 00:28:07 +0530

    auto complete updated

--- WebDriver/src/testSuite/TestAutoComplete.java
+++ WebDriver/src/testSuite/TestAutoComplete.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import main.Browser;
@@ -16,21 +18,23 @@ public class TestAutoComplete extends Browser{
 		driver.navigate().to("http://demo.automationtesting.in/AutoComplete.html");
 		driver.manage().window().maximize();
 		
+		WebDriverWait wait = new WebDriverWait(driver, 3);
+		
 	WebElement auto = driver.findElement(By.xpath("//div[contains(@class,'ui-autocomplete-multiselect')]/input"));
 	auto.sendKeys("ind");
 	
-	List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item']/a"));
+	List<WebElement> options= wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//li[@class='ui-menu-item']/a")));
 	int a = options.size();
 	System.out.println(a);
 	
-	//String expectedVal = "India";
-	/*for(WebElement autoVal:options) {
+	String expectedVal = "India";
+	for(WebElement autoVal:options) {
 		String searchedVal = autoVal.getText();
 		if(searchedVal.equalsIgnoreCase(expectedVal)) {
 			System.out.println("Found text : '"+expectedVal+"' with auto complete value 'IND'");
-		}*/
+		}
 	}
 	
- //}
+ }
 
 }
